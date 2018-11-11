1a0bed71bd1a66a21e4720afd1646062c62decec
sabotior <black_lion@mail.ru>
2018-11-11 21:36:36 +0400

    страница feedback сделал форму, добавил эффекты jQueryui

--- js/feedback.js
+++ js/feedback.js
@@ -1,46 +1,25 @@
-function showError(container, errorMessage) {
-  container.className = 'error';
-  var msgElem = document.createElement('span');
-  msgElem.className = "error-message";
-  msgElem.innerHTML = errorMessage;
-  container.appendChild(msgElem);
-}
+var validation = {
+    phone: /^\+7\(\d{3}\)\d{3}\-\d{4}$/,
+    email: /^[-._a-z0-9]+@([a-z0-9][a-z0-9]+\.)+[a-z]{2,6}$/i
+};
 
-function resetError(container) {
-  container.className = '';
-  if (container.lastChild.className == "error-message") {
-      container.removeChild(container.lastChild);
-  }
-}
+window.onload = function () {
 
-function validate(form) {
-  var elems = form.elements;
-
-  resetError(elems.fname.parentNode);
-  if (!elems.fname.value) {
-      showError(elems.fname.parentNode, ' Укажите имя.');
-  } else if (/[^a-z^а-я^ё]/gi.test(elems.fname.value)) {
-      showError(elems.fname.parentNode, ' Имя содержит только буквы.');
-  }
-
-  resetError(elems.phone.parentNode);
-  if (!elems.phone.value) {
-      showError(elems.phone.parentNode, ' Укажите телефон.');
-  } else if (!/\+7\(\d{3}\)\d{3}\-\d{4}/.test(elems.phone.value)) {
-      showError(elems.phone.parentNode, ' Телефон подчиняется шаблону +7(000)000-0000');
-  }
-
-  resetError(elems.email.parentNode);
-  if (!elems.email.value) {
-      showError(elems.email.parentNode, ' Укажите E-mail.');
-  } else if (!/^[a-z]+[a-z0-9\.+\-+]*@[a-z]+\.[a-z]+$/gi.test(elems.email.value)) {
-      showError(elems.email.parentNode, ' E-mail выглядит как ' +
-          'mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru');
-  }
-
-  resetError(elems.message.parentNode);
-  if (!elems.message.value) {
-      showError(elems.message.parentNode, ' Отсутствует текст.');
-  }
-
+    document.getElementById('submit').addEventListener('click', function (event) {
+        Object.keys(validation).forEach(function (rule) {
+            var fields = document.querySelectorAll('[data-validation-rule="' + rule + '"]');
+            fields.forEach(function (field) {
+                if (validation[rule].test(field.value)) {
+                    field.classList.remove('invalid');
+                } else {
+                    field.classList.add('invalid');
+                    $('.invalid').effect('bounce');
+                    $('body').append('<div id="dialog" title="Внимание"><p>Некоторые поля заполнены не правильно</p></div>');
+                    $('#dialog').dialog();
+                    
+                }
+            });
+        });
+        event.preventDefault();
+    });
 }
